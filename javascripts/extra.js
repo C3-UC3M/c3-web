8dc7db4054f6316abea7f58674fefcbe7f4cb44b
Cosmin Petre <cosminpetre-dev+github@proton.me>
2025-12-22 15:19:40 +0100

    Deployed a2ef494 with MkDocs version: 1.6.1

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -4,3 +4,10 @@ function loadVideo(element, videoId) {
   wrapper.innerHTML = `<iframe src="https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
   element.replaceWith(wrapper);
 }
+
+function loadPresentation(element, presentationPath) {
+  const wrapper = document.createElement('div');
+  wrapper.className = 'pdf-presentation';
+  wrapper.innerHTML = `<iframe src="/c3-web/assets/pdfjs/web/viewer.html?file=${presentationPath}#page=1&zoom=page-fit" allowfullscreen</iframe>`;
+  element.replaceWith(wrapper);
+}
